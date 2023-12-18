bf943a8733091c261f60907b5a07f0fb37f89dfc
rocioalonsolh <79716332+rocioalonsolh@users.noreply.github.com>
2023-12-18 19:29:15 +0100

    Add files via upload

---
+++ solicitud_básica.R
@@ -0,0 +1,24 @@
+# Definir la URL de la solicitud
+queryUrl <- "https://www.googleapis.com/youtube/analytics/v1/reports"
+
+# Realizar la solicitud a la API de YouTube Analytics
+response <- GET(
+  queryUrl,
+  add_headers("Authorization" = paste("Bearer", token$access_token)),
+  query = list(
+    ids = "channel==@OzleyASMR",  # Reemplaza con el ID de tu canal
+    metrics = "views,estimatedMinutesWatched",
+    dimensions = NULL,
+    sort = NULL,
+    maxResults = 10,
+    filters = NULL,
+    startDate = "2020-01-01",
+    endDate = as.character(Sys.Date()),
+    currency = NULL,
+    startIndex = NULL,
+    includeHistoricalChannelData = NULL
+  )
+)
+
+# Imprimir la respuesta
+print(content(response, "text"))
