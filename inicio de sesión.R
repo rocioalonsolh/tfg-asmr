8da3c24d1e3ae9cfce20223e67c0c5cbd38020fd
rocioalonsolh <79716332+rocioalonsolh@users.noreply.github.com>
2024-05-09 15:40:57 +0200

    Add files via upload

--- inicio de sesión.R
+++ inicio de sesión.R
@@ -7,38 +7,44 @@ con <- dbConnect(RSQLite::SQLite(), "C:/Users/Rocío/Desktop/TFG/TFG/bbdd/usuari
 
 # Define la interfaz de usuario
 ui <- fluidPage(
-  titlePanel(" ", windowTitle = "Inicio de Sesión"),
-  tags$head(
-    tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css?family=Nunito"),
-    
-    tags$style(HTML("
-      body {
-        font-family: 'Nunito', sans-serif; /* Cambio de la tipografía a Nunito */
-        background-color: #e6f3ff; /* Azul pastel */
-      }
-      .login-container {
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 20px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        background-color: white;
-      }
-      .login-title {
-        text-align: center;
-        font-size: 30px;
-        margin-bottom: 20px;
-        color: #333; /* Color de texto oscuro */
-      }
-    "))
-  ),
-  div(class = "login-container",
-      h1(class = "login-title", "ASMR Sleep"),
-      textInput("email", "Email:"),
-      passwordInput("contrasena", "Contraseña:"),
-      actionButton("login", "Iniciar sesión"),
-      p(textOutput("mensaje")),
-      uiOutput("redirect")
+  titlePanel("Inicio de Sesión", windowTitle = "Inicio de Sesión"),
+  includeCSS("estilo.css"),  # Enlace al archivo CSS externo
+  
+  # Pestañas para mostrar el contenido después de iniciar sesión
+  tabsetPanel(
+    id = "tabs",
+    tabPanel("Inicio de sesión",
+             fluidRow(
+               column(12, align = "center",
+                      h1(class = "login-title", "ASMR Sleep"),
+                      div(class = "login-container",
+                          textInput("email", "Email:"),
+                          passwordInput("contrasena", "Contraseña:"),
+                          actionButton("login", "Iniciar sesión"),
+                          p(textOutput("mensaje"))
+                      )
+               )
+             ),
+    ),
+    tabPanel("Búsqueda"),
+    tabPanel("Preferencias"),
+    tabPanel("Estadísticas", titlePanel("Registro de Sueño"),
+             sidebarLayout(
+               sidebarPanel(
+                 dateInput("fecha", "Fecha:", value = Sys.Date(), format = "dd-mm"),
+                 sliderInput("horas_sueno", "Horas de sueño:", min = 1, max = 15, value = 7),
+                 sliderInput("despertares", "Número de despertares:", min = 0, max = 15, value = 0),
+                 selectInput("sensacion", "Sensación:",
+                             choices = c("normal", "cansado", "descansado"))
+               ),
+               mainPanel(
+                 actionButton("guardar", "Guardar datos"),
+                 actionButton("ver", "Ver datos guardados"),
+                 verbatimTextOutput("mensaje"),
+                 tableOutput("datos_guardados")
+               )
+             )),
+    tabPanel("Historial")
   )
 )
 
@@ -47,9 +53,13 @@ server <- function(input, output, session) {
   
   # Función para validar las credenciales
   validar_credenciales <- function(email, contrasena) {
-    query <- sprintf("SELECT COUNT(*) FROM usuarios WHERE email='%s' AND contrasena='%s'", email, contrasena)
+    query <- sprintf("SELECT nombre FROM usuarios WHERE email='%s' AND contrasena='%s'", email, contrasena)
     result <- dbGetQuery(con, query)
-    return(result[[1]] == 1)
+    if (nrow(result) == 1) {
+      return(result[[1]])
+    } else {
+      return(NULL)
+    }
   }
   
   # Observador para el botón de inicio de sesión
@@ -57,11 +67,12 @@ server <- function(input, output, session) {
     email <- input$email
     contrasena <- input$contrasena
     
-    if (validar_credenciales(email, contrasena)) {
-      # Si las credenciales son válidas, redirigir a una nueva página en blanco
-      output$redirect <- renderUI({
-        tags$script(HTML('window.location.href = "nueva_pagina_en_blanco.html";'))
-      })
+    nombre_usuario <- validar_credenciales(email, contrasena)
+    
+    if (!is.null(nombre_usuario)) {
+      # Si las credenciales son válidas, mostrar las pestañas después de iniciar sesión
+      output$mensaje <- renderText(paste("Inicio de sesión exitoso, hola", nombre_usuario))
+      
     } else {
       # Si las credenciales no son válidas, mostrar un mensaje de error
       output$mensaje <- renderText("Credenciales no válidas. Por favor, inténtalo de nuevo.")
